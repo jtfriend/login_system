87c2c16b751a5e2f76c6179dffe92c5052e5a6cd
Joseph Friend <jt2friend@gmail.com>
2019-04-08 18:03:10 +0100

    added chartjs

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ require_once 'core/init.php';
 //flashes a message only once e.g You are now registered
 if (Session::exists('home')) {
     echo '<p>'. Session::flash('home') . '</p>';
-} 
+}
 
 $user = new User();
 
@@ -18,17 +18,13 @@ if ($user->isLoggedIn()) {
         <li><a href="logout.php">Log out</a></li>
     </ul>
 <?php
-    
+
 } else {
     echo '<p>You need to <a href="login.php">log in</a> or <a href="register.php">register</a></p>';
 }
 
 $db = new DB();
     
-// $db->insert("data", [
-//     "d_field" => "test",
-//     "d_value" => 5
-// ]);
 
 if (isset($_GET)) {
     foreach($_GET as $key => $data){
@@ -45,7 +41,7 @@ $allData = $db->getAllFromTable("data", "d_timestamp");
 echo "<table>
         <tr>
             <th>ID</th>
-            <th>Field</th> 
+            <th>Field</th>
             <th>Value</th>
             <th>Timestamp</th>
         </tr>";
@@ -59,11 +55,43 @@ echo "<table>
         </tr>";
     }
 echo "</table>";
-
-
-
-
-
 ?>
 
+<body>
+    <canvas id="chartJSContainer" width="600" height="400"></canvas>
+</body>
+<script src="JS/Chart.bundle.min.js"></script>
+<script>
 
+    var options = {
+        type: 'line',
+        data: {
+            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+            datasets: [
+                {
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                borderWidth: 1
+                },
+                    {
+                        label: '# of Points',
+                        data: [7, 11, 5, 8, 3, 7],
+                        borderWidth: 1
+                    }
+                ]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                ticks: {
+                    reverse: false
+                }
+            }]
+            }
+        }
+    }
+
+    var ctx = document.getElementById('chartJSContainer').getContext('2d');
+    new Chart(ctx, options);
+
+</script>
